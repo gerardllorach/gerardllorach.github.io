6336a02ebc59351b727a2572e531592f08fa3afd
gerardllorach <gerard.llorach@upf.edu>
2020-05-29 12:26:55 +0200

    TEST

---
+++ Fortgeschritenne/script.js
@@ -0,0 +1,286 @@
+// The main global scope
+
+// Variables
+
+startDemo = () => {
+  // Start audio context and declare variables
+  const AudioContext = window.AudioContext || window.webkitAudioContext;
+  const audioCtx = new AudioContext();
+  let soundSource;
+  let soundBuffer = {};
+  const analyser = audioCtx.createAnalyser();
+  analyser.fftSize = 2048;
+  console.log("Sampling rate: " + audioCtx.sampleRate);
+  analyser.smoothingTimeConstant = 0.0;
+  const waveBuffer = new Float32Array(analyser.fftSize);
+  const prevWaveBuffer = new Float32Array(analyser.fftSize);
+  // Load AudioWorklet
+  let vocoderNode = null;
+  let workletBuffer = null;
+  let workletBuffer2 = null;
+  audioCtx.audioWorklet.addModule('vocoder.js').then(() => {
+    console.log("Vocoder audioworklet loaded...");
+    vocoderNode = new AudioWorkletNode(audioCtx, 'vocoder');
+    window.vN = vocoderNode;
+    vocoderNode.port.onmessage = (e) => {
+      //console.log(e.data);
+      if (e.data.buffer !== undefined){
+        workletBuffer = e.data.buffer;
+        workletBuffer2 = e.data.bufferPair;
+      }
+      if (e.data.message == 'Update'){
+        console.log(e.data);
+      }
+    };
+    //oscillator.connect(bypasser).connect(context.destination);
+    //oscillator.start();
+  });
+
+
+
+  // DOM elements
+  const playButton = document.getElementById("playButton");
+  const filterButton = document.getElementById("filterButton");
+  const vocoderButton = document.getElementById("vocoderButton");
+  const selSoundContainer = document.getElementById("selSoundContainer");
+  const selectAudioList = document.getElementById("selectAudio");
+  const canvas = document.getElementById("myCanvas");
+  const canvasCtx = canvas.getContext("2d");
+  // Resize canvas
+  canvas.width = document.body.clientWidth;
+  canvas.height = document.body.clientHeight;
+
+  // App control variables
+  let playing = false;
+  let filteron = false;
+
+
+  // Vocal tract filter
+  let feedForward = [1],
+  feedBack = [1, -1.6685,    0.3762,    0.2547,    0.1319,    0.0317,   -0.0245,   -0.0428,   -0.0563,   -0.0250,    0.0000,    0.0185,    0.0356];
+  // IIR filter
+  const iirfilter = audioCtx.createIIRFilter(feedForward, feedBack);
+
+
+
+  // Button actions
+  // Play/Pause
+  playButton.onclick = () => {
+    if (playing === false) {
+      // check if context is in suspended state (autoplay policy)
+      if (audioCtx.state === 'suspended') {
+        audioCtx.resume();
+      }
+      soundSource = audioCtx.createBufferSource();
+      soundSource.buffer = soundBuffer[selectAudioList.value];
+      
+      // Destination and filtering
+      /*if (!filteron){
+        soundSource.connect(audioCtx.destination);
+      } else{
+        soundSource.connect(iirfilter).connect(audioCtx.destination);
+      }*/
+      // Vocoder destination
+      if (!vocoderButton.checked){
+        soundSource.connect(audioCtx.destination);
+        // Anayser
+        soundSource.connect(analyser);
+      } else {
+        soundSource.connect(vocoderNode).connect(audioCtx.destination);
+        // Anayser
+        soundSource.connect(vocoderNode).connect(analyser);
+      }
+
+      soundSource.start();
+      playing = true;
+      playButton.innerText = 'Pause sound';
+    } else {
+      soundSource.stop();
+      vocoderNode.disconnect();
+      playing = false;
+      playButton.innerText = 'Play sound';
+    }
+  }
+
+
+  filterButton.onclick = () => {
+    /*if (filteron == false) {
+      filteron = true;
+      if (playing){
+        soundSource.disconnect(audioCtx.destination);
+        soundSource.connect(iirfilter).connect(audioCtx.destination);
+      }
+    
+    } else {
+      filteron = false;
+      if (playing){
+        soundSource.disconnect(iirfilter);
+        soundSource.connect(audioCtx.destination);
+      }
+    }*/
+  }
+
+  vocoderButton.onclick = pathVocoder = () => {
+    if (vocoderButton.checked){
+      if (playing){
+        soundSource.disconnect(audioCtx.destination);
+        soundSource.disconnect(analyser);
+        soundSource.connect(vocoderNode).connect(audioCtx.destination);
+        soundSource.connect(vocoderNode).connect(analyser);
+      }
+    } else {
+      if (playing) {
+        vocoderNode.disconnect(audioCtx.destination);
+        soundSource.disconnect(vocoderNode);
+        soundSource.connect(audioCtx.destination);
+        soundSource.connect(analyser);
+      }
+    }
+  }
+
+
+
+
+
+
+  function paintWave(inBuffer){
+
+    // Scale of wave
+    let stepW = 0.3*canvas.width/inBuffer.length;
+    let stepH = 100;
+    // Axis
+    canvasCtx.lineWidth = "1";
+    canvasCtx.strokeStyle = "rgba(255,255,255, 0.5)";
+    canvasCtx.beginPath(); // X axis
+    canvasCtx.moveTo(0, 0);
+    canvasCtx.lineTo(1.1*inBuffer.length*stepW, 0);
+    canvasCtx.stroke();
+    canvasCtx.beginPath(); // Y axis
+    canvasCtx.moveTo(0, stepH*1.1);
+    canvasCtx.lineTo(0, -stepH*1.1);
+    canvasCtx.stroke();
+
+    if (!playing)
+      return;
+
+    // Wave signal
+    canvasCtx.beginPath();
+    canvasCtx.lineWidth = "1";
+    canvasCtx.strokeStyle = "white";
+    canvasCtx.moveTo(0, 0);
+    for (let i = 0; i< inBuffer.length; i++){
+      canvasCtx.lineTo(i*stepW, inBuffer[i]*stepH);
+    }
+    canvasCtx.stroke();
+  }
+
+
+
+  // Paint loop
+  function draw(dt)
+  {
+    // Clear canvas
+    canvasCtx.clearRect(0,0, canvas.width, canvas.height);
+
+    // Get wave buffer
+    if (playing)
+      analyser.getFloatTimeDomainData(waveBuffer);
+
+    // Paint wave signal
+    let wposW = 100;
+    let wposH = 100;
+    canvasCtx.translate(wposW,wposH);
+    paintWave(waveBuffer);
+    canvasCtx.translate(-wposW,-wposH);
+
+    // Paint AudioworkletBuffer
+    if (workletBuffer !== null && workletBuffer !== undefined){
+      wposW = 100;
+      wposH = 500;
+      canvasCtx.translate(wposW,wposH);
+      paintWave(workletBuffer);
+      canvasCtx.translate(-wposW,-wposH);
+
+      wposW = canvas.height/2;
+      wposH = 500;
+      canvasCtx.translate(wposW,wposH);
+      paintWave(workletBuffer2);
+      canvasCtx.translate(-wposW,-wposH);
+    }
+
+
+      requestAnimationFrame(draw);
+  }
+  requestAnimationFrame(draw);
+
+
+
+
+
+
+
+
+  canvas.ondragover = function(e) {
+    e.preventDefault()
+    e.stopPropagation()
+  }
+
+  canvas.ondrop = function(e) {
+      e.preventDefault()
+      e.stopPropagation()
+
+      var files = e.dataTransfer.files;
+      // Load files
+      var count = 0;
+      for (var i = 0; i < files.length; i++) {
+          var file = files[i];
+
+          var reader = new FileReader();
+          reader.fname = file.name;
+
+          // Load files 
+          reader.addEventListener('load', function(e) {
+              var data = e.target.result;
+            var fileName = this.fname;
+            var ss = fileName.split(".");
+            var extension = ss[ss.length - 1];
+            var sName = ss[0];
+
+            // For audio files
+            if (extension == "wav"){
+                audioCtx.decodeAudioData(data, function(buffer) {
+                  // Define audio buffer
+                  soundBuffer[sName] = buffer;
+                  // Add element to DOM list
+                  selectAudio.add(new Option(sName, sName, false,sName));
+                });
+          }
+
+          // Count the number of files loaded
+          count ++;
+          if (count == files.length){
+            // All files loaded
+            console.log(count + ' files dropped', 'success', 3);
+            
+          }
+          })
+          reader.readAsArrayBuffer(file);
+      }
+  }
+
+
+
+  // Resize canvas
+  window.onresize = function() {
+    canvas.width = document.body.clientWidth;
+    canvas.height = document.body.clientHeight;
+  }
+
+}
+
+
+
+
+window.onload = () => {
+  startDemo();
+}
---
+++ Fortgeschritenne/vocoder.js
@@ -0,0 +1,178 @@
+/* Vocoder AudioWorklet Processor
+
+ - Overlap and Add:
+  -- Make frames (current and previous) out of the blocks of the 128 samples
+  -- Add frames to produce output
+
+
+*/
+class Vocoder extends AudioWorkletProcessor {
+
+  // currentFrame, currentTime and sampleRate are global variables of AudioWorkletProcessor
+  // TODO: check if currentFrame is always increasing (some frames might be processed twice?)
+  constructor() {
+    super();
+    this._lastUpdate = currentTime;
+
+    // Frame size (20ms)
+    const fSize = 0.02*sampleRate; 
+    // Make the framesize multiple of 128 (audio render block size)
+    this._frameSize = 128*Math.round(fSize/128); // Frame duration = this._frameSize/sampleRate;
+    this._numBlocksInFrame = this._frameSize/128; // 8 at 48kHz and 20ms window
+    // 50% overlap
+    this._numBlocksOverlap = Math.floor(this._numBlocksInFrame/2); // 4 at 48kHz and 20ms window
+    
+    // Define frame buffers
+    this._oddBuffer = new Float32Array(this._frameSize); // previous and current are reused
+    this._pairBuffer = new Float32Array(this._frameSize); //  previous and current are reused
+
+    // We want to reuse the two buffers. This part is a bit complicated and requires a detailed description
+    // Finding the block indices that belong to each buffer is complicated 
+    // for buffers with an odd num of blocks.
+    // Instead of using full blocks, half blocks could be used. This also adds
+    // another layer of complexity, so not much to gain...
+    // Module denominator to compute the block index
+    this._modIndexBuffer = this._numBlocksInFrame + Math.ceil(this._numBlocksInFrame/2) - Math.floor(this._numBlocksInFrame/2); // Adds 1 to numBlocksInFrame if it's odd, otherwise adds 0
+
+    // Count blocks
+    this._countBlock = 0;
+
+
+    // Computed buffers
+    this._oddSynthBuffer = new Float32Array(this._frameSize);
+    this._pairSynthBuffer = new Float32Array(this._frameSize);
+  }
+
+
+
+
+  // Fill buffers
+  processBlock(outBlock, inputBlock) {
+
+    /*
+    Example of frames of made of 5 blocks
+      O O O O O -- blockPair
+            0 0 0 0 0 -- blockOdd
+                  O O O O O -- blockPair
+                        0 0 0 0 0
+
+      0 1 2 3 4 - time, i.e. block count (blockPair)
+            3 4 5 6 7 - time, i.e. block count (blockOdd)
+            0 1 2 3 4 - ind blockOdd
+    */
+    let indBlockPair = this._countBlock % this._modIndexBuffer;
+    if (indBlockPair <= this._numBlocksInFrame) // Only applies for odd numBlocksInFrame (a block is only assigned to a buffer in the middle of the frame)
+      this._pairBuffer.set(inputBlock, 128*indBlockPair); 
+
+
+    let indBlockOdd = (indBlockPair + this._modIndexBuffer/2) % this._modIndexBuffer;
+    if (indBlockOdd <= this._numBlocksInFrame) // Only applies for odd numBlocksInFrame (a block is only assigned to a buffer in the middle of the frame)
+      this._oddBuffer.set(inputBlock, 128*indBlockOdd);
+
+    // Add: Get the output block from the mix of pairSynthBuff and oddSynthBuff
+    this.synthesizeOutputBlock(outBlock, inputBlock);
+
+
+    // Synthesize buffers -- Do modifications on the buffers (vocoder goes here)
+    this.synthesizeBuffer(indBlockPair, this._pairBuffer, this._pairSynthBuffer);
+    this.synthesizeBuffer(indBlockOdd, this._oddBuffer, this._oddSynthBuffer);
+  }
+
+
+
+
+  // Synthesize buffer
+  synthesizeBuffer(indBlock, buffer, synthBuffer) {
+    // Only synthesize when it is filled
+    if (indBlock == this._numBlocksInFrame - 1){
+      for (let i = 0; i < buffer.length; i++){
+        // Do something
+        //synthBuffer[i] = buffer[i];
+        // Smooth
+        if (i == 0){// Skip first sample
+          synthBuffer[i] = buffer[i];
+          continue;
+        }
+        // EMA
+        synthBuffer[i] = buffer[i]*0.01 + synthBuffer[i-1]*0.99;
+      }
+    }
+  }
+
+
+  // Windowing and mixing odd and pair buffers
+  synthesizeOutputBlock(outBlock, inputBlock) {
+
+    let indBlockPair = this._countBlock % this._modIndexBuffer;
+    let indBlockOdd = (indBlockPair + this._modIndexBuffer/2) % this._modIndexBuffer;
+
+    // TODO: add hanning window here
+    for (let i = 0; i<outBlock.length; i++){
+      let indPair = i + 128*indBlockPair;
+      let indOdd = i + 128*indBlockOdd;
+
+      // Hanning values
+
+      outBlock[i] = 0.5*this._pairSynthBuffer[indPair] +
+                    0.5*this._oddSynthBuffer[indOdd];
+    }
+
+  }
+
+
+
+
+  process(inputs, outputs) {
+    // By default, the node has single input and output.
+    const input = inputs[0];
+    const output = outputs[0];
+
+    for (let channel = 0; channel < output.length; ++channel) {
+
+      const inputChannel = input[channel];
+      const outputChannel = output[channel];
+      for (let i = 0; i < inputChannel.length; ++i){
+        // Distortion
+        outputChannel[i] = Math.max(-1, Math.min(1,inputChannel[i]*5)) ; // Amplify and clamp       
+      }
+
+      // Fill buffers (overlapped)
+      // Add buffers
+      // Modify buffers
+      this.processBlock(outputChannel, inputChannel);
+    }
+
+    this._countBlock++;
+
+
+    // Send to main thread the buffers every frame
+    if (this._countBlock  % this._modIndexBuffer == this._numBlocksInFrame-1){
+      this.port.postMessage({
+        buffer: this._oddSynthBuffer.slice(),
+        bufferPair: this._pairSynthBuffer.slice(),
+      });
+       
+    }
+
+
+    // Send data
+    // Post a message to the node for every 1 second.
+    if (currentTime - this._lastUpdate > 1.0) {
+      this.port.postMessage({
+        message: 'Update',
+        contextTimestamp: currentTime,
+        currentFrame: currentFrame,
+        currentBlock: this._countBlock,
+        buffer: this._oddSynthBuffer.slice(),
+        bufferPair: this._pairSynthBuffer.slice(),
+      });
+      this._lastUpdate = currentTime;
+      //this._oddBuffer.fill(0);
+    }
+
+
+    return true;
+  }
+}
+
+registerProcessor('vocoder', Vocoder);
